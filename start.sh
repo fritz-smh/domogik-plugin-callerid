13ef04dcdbee12eadbdeab1c743fca6a7fa90678
fritz-smh <fritz.smh@gmail.com>
2015-12-01 18:33:18 +0100

    start

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#export PYTHONPATH=/var/lib/domogik && /usr/bin/python bin/callerid.py -f
-export PYTHONPATH=/var/lib/domogik && /usr/bin/python bin/callerid.py -f -T ./tests/data.json
+export PYTHONPATH=/var/lib/domogik && /usr/bin/python bin/callerid.py -f
+#export PYTHONPATH=/var/lib/domogik && /usr/bin/python bin/callerid.py -f -T ./tests/data.json
